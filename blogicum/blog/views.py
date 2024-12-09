b5c169f266fa361cf213927467ee2be70770f04d
Никита Рудь <nikita.rud.20015@mail.ru>
2024-12-09 11:27:33 +1000

    k

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,25 +1,19 @@
-from django.shortcuts import render
-from django.utils.timezone import make_aware
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from blog.models import Post, Category
 from django.db.models import Q
-import datetime
-from django.shortcuts import render
-from django.db.models import Q
-import datetime
+from django.utils import timezone
 from .models import Post
 
 
 def index(request):
-    today_date = datetime.date.today()
-
     post_list = Post.objects.select_related(
         'category', 'location', 'author'
     ).filter(
         Q(is_published=True) & 
-        Q(pub_date__date__lt=today_date) & 
+        Q(pub_date__lte=timezone.now()) & 
         Q(category__is_published=True)  # Фильтруем по опубликованной категории
-    ).order_by('-pub_date')[1:6]
+    ).order_by('-pub_date')[:5]
 
     # Словарь контекста
     context = {
@@ -31,56 +25,42 @@ def index(request):
     return render(request, template_name, context)
 
 
-def post_detail(request, id):
-    try:
-        # Фильтрация на уровне базы данных
-        post = Post.objects.filter(
-            id=id,
-            is_published=True,
-            pub_date__lte=make_aware(datetime.datetime.now()),
-            category__is_published=True
-        ).first()
+def post_detail(request, id):  
+    post = get_object_or_404(
+        Post.objects.select_related('author', 'category', 'location'),
+        id=id,
+        is_published=True
+    )
 
-        if not post:
-            raise Http404("Пост не найден или снят с публикации")
+    if not post.category.is_published:
+        raise Http404("Category is not published.")
 
-    except Post.DoesNotExist:
-        raise Http404("Пост не найден")
+    if post.pub_date > timezone.now():
+        raise Http404("Post is scheduled for the future.")
 
-    context = {
-        'post': post
-    }
-    template_name = 'blog/detail.html'
-
-    return render(request, template_name, context)
+    context = {'post': post}
+    return render(request, 'blog/detail.html', context)
 
 
 def category_posts(request, category_slug):
-    today_date = datetime.date.today()
-
-    # Попытка найти категорию по slug
-    try:
-        category = Category.objects.get(slug=category_slug)
-    except Category.DoesNotExist:
-        raise Http404("Категория не найдена")
-
-    # Если категория не опубликована, возвращаем ошибку 404
-    if not category.is_published:
-        raise Http404("Категория не опубликована")
-
-    # Фильтруем посты по категории и датам
-    post_list = Post.objects.select_related(
-        'category', 'location', 'author'
-    ).filter(
-        Q(is_published=True) &
-        Q(pub_date__date__lt=today_date) &
-        Q(category__slug=category_slug)
+    category = get_object_or_404(
+        Category.objects.filter(is_published=True),
+        slug=category_slug
     )
 
-    context = {
-        'post_list': post_list,
-        'category': category,
-    }
-    template_name = 'blog/category.html'
+    current_time_local = timezone.localtime(timezone.now())
 
-    return render(request, template_name, context)
+    # category.save()
+
+    posts = Post.objects.filter(
+        category=category,
+        is_published=True,
+        pub_date__lte=current_time_local
+    ).order_by('-pub_date')
+
+    context = {
+        'category': category,
+        'post_list': posts,
+    }
+
+    return render(request, 'blog/category.html', context)
